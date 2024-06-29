7af6342fec48fdbb697058c006a32284f8cd229f
Youssab El Khanagry <subamichael28@gmail.com>
2024-06-29 20:15:18 +0300

    adding state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:expensestracker/screen/home_page.dart';
 import 'package:expensestracker/database/expense_database.dart';
 import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
