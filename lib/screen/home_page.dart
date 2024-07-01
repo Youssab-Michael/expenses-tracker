f4d51c427dcfe11294e3a2e62ac718bfcabeac69
Youssab El Khanagry <subamichael28@gmail.com>
2024-07-01 15:36:39 +0300

    Expenses Dialog modified

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:expensestracker/model/expense.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,13 +9,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController amountController = TextEditingController();
 
   void openNewExpenseBox() {
     showDialog(
       context: context,
       builder: (context) => AlertDialog(
         title: Text("New Expense"),
-
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: InputDecoration(hintText: "Name"),
+            ),
+            TextField(
+              controller: amountController,
+              decoration: InputDecoration(hintText: "Amount"),
+            ),
+          ],
+        ),
+        actions: [
+          _cancelButton(),
+        ],
       ),
     );
   }
@@ -26,6 +44,34 @@ class _HomePageState extends State<HomePage> {
         onPressed: openNewExpenseBox,
         child: Icon(Icons.add),
       ),
+    );
+  }
+
+  Widget _cancelButton() {
+    return MaterialButton(
+      onPressed: () {
+        Navigator.pop(context);
+        nameController.clear();
+        amountController.clear();
+      },
+      child: Text("Cancel"),
+    );
+  }
+
+  Widget _createNewExpenseButton() {
+    return MaterialButton(
+      onPressed: () {
+        if (nameController.text.isNotEmpty &&
+            amountController.text.isNotEmpty) {
+          Navigator.pop(context);
+          Expense newExpense = Expense(
+            name: nameController.text,
+            amount: amountController.text,
+            date: DateTime.now(),
+          );
+        }
+      },
+      child: Text("Save"),
     );
   }
 }
